2f6c7c2149cc6d7946f475825aff1e3c86cf7d0f
Md. Abdul Alim <32503438+alim84@users.noreply.github.com>
2025-02-08 20:44:39 +0600

    Tostify and loader

--- src/pages/AddCategory.jsx
+++ src/pages/AddCategory.jsx
@@ -1,14 +1,17 @@
 import { useState } from "react";
 import axios, { toFormData } from "axios";
+import { Spinner } from "@material-tailwind/react";
+import { toast, Bounce } from "react-toastify";
 
 const AddCategory = () => {
   const [categoryName, setCategoryName] = useState("");
   const [categoryDescription, setCategoryDescription] = useState("");
   const [categoryImage, setCategoryImage] = useState(null);
+  const [loader, setLoader] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
+    setLoader(true);
     let data = new FormData();
     data.append("name", categoryName);
     data.append("description", categoryDescription);
@@ -23,16 +26,31 @@ const AddCategory = () => {
         },
       })
       .then((data) => {
+        toast.success(data ? data.data.msg : "category creted successfull", {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: false,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+          transition: Bounce,
+        });
+        setLoader(false);
+
         console.log(data);
-      }).catch((error)=>{
-        console.log(error)
       })
+      .catch((error) => {
+        setLoader(false);
+        console.log(error);
+      });
     // {
     //   categoryName, categoryDescription, categoryImage;
     // }
   };
-  console.log(categoryName)
-  console.log(categoryDescription)
+  console.log(categoryName);
+  console.log(categoryDescription);
   return (
     <div className="mx-auto w-full max-w-4xl mt-10 pt-6 bg-white rounded-lg shadow-lg p-8">
       <h2 className="text-3xl font-semibold text-center text-gray-800 mb-6">
@@ -50,7 +68,6 @@ const AddCategory = () => {
             id="categoryName"
             value={categoryName}
             onChange={(e) => setCategoryName(e.target.value)}
-            
             placeholder="Enter category name"
             className="mt-2 p-4 border border-gray-300 rounded-lg shadow-sm focus:ring-2 focus:ring-indigo-500 transition-all duration-300"
             required
@@ -92,13 +109,19 @@ const AddCategory = () => {
         </div>
 
         {/* Submit Button */}
-        <div className="flex justify-center">
-          <button
-            type="submit"
-            className="px-10 py-3 mt-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none transition-all duration-300"
-          >
-            Add Category
-          </button>
+        <div className="flex relative justify-center">
+          {loader ? (
+            <div className="absolute top-6">
+              <Spinner className="w-[45px] h-[25px] " color="red" />
+            </div>
+          ) : (
+            <button
+              type="submit"
+              className="px-10 py-3 mt-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 focus:outline-none transition-all duration-300"
+            >
+              Add Category
+            </button>
+          )}
         </div>
       </form>
     </div>
